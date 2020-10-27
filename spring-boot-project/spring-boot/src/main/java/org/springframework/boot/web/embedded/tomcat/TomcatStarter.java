941e74eb6bca2f927c721d76762f03cd50857484
sunjinwei <sunjinwei5828@163.com>
2020-10-27 16:18:37 +0800

    内嵌tomcat不会以spi方式加载ServletContainerInitializer，而是用TomcatStarter的onStartup，间接启动ServletContextInitializers，来达到ServletContainerInitializer的效果。

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/tomcat/TomcatStarter.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/tomcat/TomcatStarter.java
@@ -16,18 +16,24 @@
 
 package org.springframework.boot.web.embedded.tomcat;
 
-import java.util.Set;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+import java.util.Set;
 
 /**
+ * TomcatStarter实现了ServletContainerInitializer，
+ * 重写的onstartup方法，这里会启动ServletContextInitializer的onStartup方法.
+ * <p>
+ * 内嵌的tomcat不会以spi方式加载ServletContainerInitializer，
+ * 而是用TomcatStarter的onStartup，间接启动ServletContextInitializers，来达到ServletContainerInitializer的效果。
+ * <p>
+ * 类似SpringServletContainerInitializer老大哥带WebApplicationInitializer的套路，
+ * <p>
  * {@link ServletContainerInitializer} used to trigger {@link ServletContextInitializer
  * ServletContextInitializers} and track startup errors.
  *
@@ -36,37 +42,39 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
  */
 class TomcatStarter implements ServletContainerInitializer {
 
-	private static final Log logger = LogFactory.getLog(TomcatStarter.class);
+    private static final Log logger = LogFactory.getLog(TomcatStarter.class);
 
-	private final ServletContextInitializer[] initializers;
+    private final ServletContextInitializer[] initializers;
 
-	private volatile Exception startUpException;
+    private volatile Exception startUpException;
 
-	TomcatStarter(ServletContextInitializer[] initializers) {
-		this.initializers = initializers;
-	}
+    TomcatStarter(ServletContextInitializer[] initializers) {
+        this.initializers = initializers;
+    }
 
-	@Override
-	public void onStartup(Set<Class<?>> classes, ServletContext servletContext)
-			throws ServletException {
-		try {
-			for (ServletContextInitializer initializer : this.initializers) {
-				initializer.onStartup(servletContext);
-			}
-		}
-		catch (Exception ex) {
-			this.startUpException = ex;
-			// Prevent Tomcat from logging and re-throwing when we know we can
-			// deal with it in the main thread, but log for information here.
-			if (logger.isErrorEnabled()) {
-				logger.error("Error starting Tomcat context. Exception: "
-						+ ex.getClass().getName() + ". Message: " + ex.getMessage());
-			}
-		}
-	}
+    @Override
+    public void onStartup(Set<Class<?>> classes, ServletContext servletContext)
+            throws ServletException {
+        try {
+            /**
+             * 内嵌tomcat如何启动ServletContextInitializer的onStartUp方法
+             */
+            for (ServletContextInitializer initializer : this.initializers) {
+                initializer.onStartup(servletContext);
+            }
+        } catch (Exception ex) {
+            this.startUpException = ex;
+            // Prevent Tomcat from logging and re-throwing when we know we can
+            // deal with it in the main thread, but log for information here.
+            if (logger.isErrorEnabled()) {
+                logger.error("Error starting Tomcat context. Exception: "
+                        + ex.getClass().getName() + ". Message: " + ex.getMessage());
+            }
+        }
+    }
 
-	public Exception getStartUpException() {
-		return this.startUpException;
-	}
+    public Exception getStartUpException() {
+        return this.startUpException;
+    }
 
 }
