263cb0ce30d4f1b4024870158e44a4f8296781c7
sunjinwei <sunjinwei5828@163.com>
2020-10-28 09:59:42 +0800

    springboot自己提供的ServletContextInitializer，功能和WebApplicationInitializer一样，区别就是前者由spring管理，后者由tomcat管理

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletContextInitializer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/ServletContextInitializer.java
@@ -16,38 +16,45 @@
 
 package org.springframework.boot.web.servlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 /**
+ * 该接口是由springboot提供的，但是不像WebApplicationInitializer，会由tomcat管理。
+ * ServletContextInitializer接口不会被SpringServletContainerInitializer【ServletContainerInitializer】检测到。
+ * 生命周期由spring管理，而不是servlet 管理。
+ * <p>
  * Interface used to configure a Servlet 3.0+ {@link ServletContext context}
  * programmatically. Unlike {@link WebApplicationInitializer}, classes that implement this
  * interface (and do not implement {@link WebApplicationInitializer}) will <b>not</b> be
  * detected by {@link SpringServletContainerInitializer} and hence will not be
  * automatically bootstrapped by the Servlet container.
  * <p>
+ * 因为设计者，就是不让内嵌容器支持ServletContainerInitializer。
+ *
  * This interface is primarily designed to allow {@link ServletContextInitializer}s to be
  * managed by Spring and not the Servlet container.
  * <p>
  * For configuration examples see {@link WebApplicationInitializer}.
  *
  * @author Phillip Webb
- * @since 1.4.0
  * @see WebApplicationInitializer
+ * @since 1.4.0
  */
 @FunctionalInterface
 public interface ServletContextInitializer {
 
-	/**
-	 * Configure the given {@link ServletContext} with any servlets, filters, listeners
-	 * context-params and attributes necessary for initialization.
-	 * @param servletContext the {@code ServletContext} to initialize
-	 * @throws ServletException if any call against the given {@code ServletContext}
-	 * throws a {@code ServletException}
-	 */
-	void onStartup(ServletContext servletContext) throws ServletException;
+    /**
+     * Configure the given {@link ServletContext} with any servlets, filters, listeners
+     * context-params and attributes necessary for initialization.
+     *
+     * @param servletContext the {@code ServletContext} to initialize
+     * @throws ServletException if any call against the given {@code ServletContext}
+     *                          throws a {@code ServletException}
+     */
+    void onStartup(ServletContext servletContext) throws ServletException;
 
 }
