511d4a43365f3a2b4aa035ae22100cd6c3a82bd1
sunjinwei <sunjinwei5828@163.com>
2021-06-29 06:32:50 +0800

    xml/properties/yml/yaml结尾的配置文件的加载器

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/PropertiesPropertySourceLoader.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/PropertiesPropertySourceLoader.java
@@ -16,16 +16,18 @@
 
 package org.springframework.boot.env;
 
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-
 /**
+ * 支持配置文件为xml和properties
+ * <p>
  * Strategy to load '.properties' files into a {@link PropertySource}.
  *
  * @author Dave Syer
@@ -34,31 +36,31 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
  */
 public class PropertiesPropertySourceLoader implements PropertySourceLoader {
 
-	private static final String XML_FILE_EXTENSION = ".xml";
+    private static final String XML_FILE_EXTENSION = ".xml";
 
-	@Override
-	public String[] getFileExtensions() {
-		return new String[] { "properties", "xml" };
-	}
+    @Override
+    public String[] getFileExtensions() {
+        return new String[]{"properties", "xml"};
+    }
 
-	@Override
-	public List<PropertySource<?>> load(String name, Resource resource)
-			throws IOException {
-		Map<String, ?> properties = loadProperties(resource);
-		if (properties.isEmpty()) {
-			return Collections.emptyList();
-		}
-		return Collections
-				.singletonList(new OriginTrackedMapPropertySource(name, properties));
-	}
+    @Override
+    public List<PropertySource<?>> load(String name, Resource resource)
+            throws IOException {
+        Map<String, ?> properties = loadProperties(resource);
+        if (properties.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return Collections
+                .singletonList(new OriginTrackedMapPropertySource(name, properties));
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Map<String, ?> loadProperties(Resource resource) throws IOException {
-		String filename = resource.getFilename();
-		if (filename != null && filename.endsWith(XML_FILE_EXTENSION)) {
-			return (Map) PropertiesLoaderUtils.loadProperties(resource);
-		}
-		return new OriginTrackedPropertiesLoader(resource).load();
-	}
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private Map<String, ?> loadProperties(Resource resource) throws IOException {
+        String filename = resource.getFilename();
+        if (filename != null && filename.endsWith(XML_FILE_EXTENSION)) {
+            return (Map) PropertiesLoaderUtils.loadProperties(resource);
+        }
+        return new OriginTrackedPropertiesLoader(resource).load();
+    }
 
 }
--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
@@ -16,17 +16,19 @@
 
 package org.springframework.boot.env;
 
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ClassUtils;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.Resource;
-import org.springframework.util.ClassUtils;
-
 /**
+ * 支持yml和yaml结尾
+ *
  * Strategy to load '.yml' (or '.yaml') files into a {@link PropertySource}.
  *
  * @author Dave Syer
@@ -35,29 +37,29 @@ import org.springframework.util.ClassUtils;
  */
 public class YamlPropertySourceLoader implements PropertySourceLoader {
 
-	@Override
-	public String[] getFileExtensions() {
-		return new String[] { "yml", "yaml" };
-	}
+    @Override
+    public String[] getFileExtensions() {
+        return new String[]{"yml", "yaml"};
+    }
 
-	@Override
-	public List<PropertySource<?>> load(String name, Resource resource)
-			throws IOException {
-		if (!ClassUtils.isPresent("org.yaml.snakeyaml.Yaml", null)) {
-			throw new IllegalStateException("Attempted to load " + name
-					+ " but snakeyaml was not found on the classpath");
-		}
-		List<Map<String, Object>> loaded = new OriginTrackedYamlLoader(resource).load();
-		if (loaded.isEmpty()) {
-			return Collections.emptyList();
-		}
-		List<PropertySource<?>> propertySources = new ArrayList<>(loaded.size());
-		for (int i = 0; i < loaded.size(); i++) {
-			String documentNumber = (loaded.size() != 1) ? " (document #" + i + ")" : "";
-			propertySources.add(new OriginTrackedMapPropertySource(name + documentNumber,
-					loaded.get(i)));
-		}
-		return propertySources;
-	}
+    @Override
+    public List<PropertySource<?>> load(String name, Resource resource)
+            throws IOException {
+        if (!ClassUtils.isPresent("org.yaml.snakeyaml.Yaml", null)) {
+            throw new IllegalStateException("Attempted to load " + name
+                    + " but snakeyaml was not found on the classpath");
+        }
+        List<Map<String, Object>> loaded = new OriginTrackedYamlLoader(resource).load();
+        if (loaded.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<PropertySource<?>> propertySources = new ArrayList<>(loaded.size());
+        for (int i = 0; i < loaded.size(); i++) {
+            String documentNumber = (loaded.size() != 1) ? " (document #" + i + ")" : "";
+            propertySources.add(new OriginTrackedMapPropertySource(name + documentNumber,
+                    loaded.get(i)));
+        }
+        return propertySources;
+    }
 
 }
