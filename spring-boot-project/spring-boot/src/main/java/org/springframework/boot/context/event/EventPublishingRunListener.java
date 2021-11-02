749a16679e0d36c27d7c7ff60c6bd0b5496adf88
sunjinwei <sunjinwei5828@163.com>
2021-11-02 19:26:44 +0800

    SpringApplicationRunListener：EventPublishingRunListener监听器接口，在启动流程的不同阶段会发布不同的event事件

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/event/EventPublishingRunListener.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/event/EventPublishingRunListener.java
@@ -32,10 +32,12 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.ErrorHandler;
 
 /**
- * SpringApplicationRunListener仅有一个实现类: EventPublishingRunListener,
- * EventPublishingRunListener是一个事件发布器, 它构造出不同的Event, 通过initialMulticaster广播出去,
- * SpringApplicationRunListeners与SpringApplicationRunListener生命周期相同，调用每个周期的各个SpringApplicationRunListener。
- * 然后广播相应的事件到Spring框架的ApplicationListener。
+ * SpringApplicationRunListener仅有一个实现类: EventPublishingRunListener, 需要在spring.factories文件配置
+ * 核心点：
+ * 1.EventPublishingRunListener是一个事件发布器, 在启动过程的不同阶段 它构造出不同的Event, 通过initialMulticaster广播出去,
+ * 2.SpringApplicationRunListeners与SpringApplicationRunListener生命周期相同，
+ * 3.调用每个周期的各个SpringApplicationRunListener。
+ * 4.然后广播相应的事件到Spring框架的ApplicationListener。
  *
  * {@link SpringApplicationRunListener} to publish {@link SpringApplicationEvent}s.
  * <p>
@@ -55,10 +57,23 @@ public class EventPublishingRunListener implements SpringApplicationRunListener,
 
     private final SimpleApplicationEventMulticaster initialMulticaster;
 
-    public EventPublishingRunListener(SpringApplication application, String[] args) {
+	/**
+	 * 构造函数做的事情就是将spring.factories文件的ApplicationListener监听器添加到多播器中
+	 * 这里就将SpringApplicationRunListener监听器和spring的ApplicationListener关联起来了
+	 *
+	 * @param application
+	 * @param args
+	 */
+	public EventPublishingRunListener(SpringApplication application, String[] args) {
         this.application = application;
         this.args = args;
-        this.initialMulticaster = new SimpleApplicationEventMulticaster();
+		/**
+		 * 重要1：这个多播器就是ApplicationEventMulticaster，会在spring的refresh方法中
+		 */
+		this.initialMulticaster = new SimpleApplicationEventMulticaster();
+		/**
+		 * 重要2：这里会spring.factories的ApplicationListener添加到初始化多播器中
+		 */
         for (ApplicationListener<?> listener : application.getListeners()) {
             this.initialMulticaster.addApplicationListener(listener);
         }
@@ -69,14 +84,16 @@ public class EventPublishingRunListener implements SpringApplicationRunListener,
         return 0;
     }
 
-    @Override
+	/**
+	 * 1发布一个ApplicationStartingEvent的event事件
+	 */
+	@Override
     public void starting() {
-        this.initialMulticaster.multicastEvent(
-                new ApplicationStartingEvent(this.application, this.args));
+        this.initialMulticaster.multicastEvent(new ApplicationStartingEvent(this.application, this.args));
     }
 
     /**
-     * 我们跟进EventPublishingRunListener监听器的environmentPrepared方法
+	 * 2发布一个ApplicationEnvironmentPreparedEvent的event事件
      * @param environment the environment
      */
     @Override
@@ -89,13 +106,20 @@ public class EventPublishingRunListener implements SpringApplicationRunListener,
         this.initialMulticaster.multicastEvent(event);
     }
 
-    @Override
+	/**
+	 * 3发布一个ApplicationContextInitializedEvent事件
+	 * @param context the application context
+	 */
+	@Override
     public void contextPrepared(ConfigurableApplicationContext context) {
-        this.initialMulticaster.multicastEvent(new ApplicationContextInitializedEvent(
-                this.application, this.args, context));
+        this.initialMulticaster.multicastEvent(new ApplicationContextInitializedEvent(this.application, this.args, context));
     }
 
-    @Override
+	/**
+	 * 4 发布一个ApplicationPreparedEvent的event事件
+	 * @param context the application context
+	 */
+	@Override
     public void contextLoaded(ConfigurableApplicationContext context) {
         for (ApplicationListener<?> listener : this.application.getListeners()) {
             if (listener instanceof ApplicationContextAware) {
@@ -103,26 +127,36 @@ public class EventPublishingRunListener implements SpringApplicationRunListener,
             }
             context.addApplicationListener(listener);
         }
-        this.initialMulticaster.multicastEvent(
-                new ApplicationPreparedEvent(this.application, this.args, context));
+        this.initialMulticaster.multicastEvent(new ApplicationPreparedEvent(this.application, this.args, context));
     }
 
-    @Override
+	/**
+	 * 5 发布一个ApplicationStartedEvent事件
+	 * @param context the application context.
+	 */
+	@Override
     public void started(ConfigurableApplicationContext context) {
-        context.publishEvent(
-                new ApplicationStartedEvent(this.application, this.args, context));
+        context.publishEvent(new ApplicationStartedEvent(this.application, this.args, context));
     }
 
-    @Override
+	/**
+	 * 6 发布一个ApplicationReadyEvent事件
+	 * @param context the application context.
+	 */
+	@Override
     public void running(ConfigurableApplicationContext context) {
-        context.publishEvent(
-                new ApplicationReadyEvent(this.application, this.args, context));
+        context.publishEvent(new ApplicationReadyEvent(this.application, this.args, context));
     }
 
-    @Override
+	/**
+	 * 7 发布ApplicationFailedEvent事件
+	 * @param context   the application context or {@code null} if a failure occurred before
+	 *                  the context was created
+	 * @param exception the failure
+	 */
+	@Override
     public void failed(ConfigurableApplicationContext context, Throwable exception) {
-        ApplicationFailedEvent event = new ApplicationFailedEvent(this.application,
-                this.args, context, exception);
+        ApplicationFailedEvent event = new ApplicationFailedEvent(this.application, this.args, context, exception);
         if (context != null && context.isActive()) {
             // Listeners have been registered to the application context so we should
             // use it at this point if we can
